522d446549c4d26ab0401e9b0fefc3fe319a8bb4
Zaki Ahmad <zakiahmad42661@gmail.com>
2023-09-20 15:58:22 -0400

    created section divider and animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Intro from "@/components/Intro"
+import SectionDivider from "@/components/SectionDivider"
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro/>
+      <SectionDivider/>
     </main>
   )
 }
