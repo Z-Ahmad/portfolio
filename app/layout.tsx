aa24dbd2b9668dbc12cf8e2c28d422ec32371912
Zaki Ahmad <zakiahmad42661@gmail.com>
2023-09-19 23:09:41 -0400

    added background blur, favicon, dummy data, resume

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+
+        <div className='bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] 
+        h-[31.24rem] w-[31.24rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem]'></div>
+
+        <div className='bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[-35rem] 
+        h-[31.24rem] w-[50rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <div></div>
+
+        {children}
+      </body>
     </html>
   )
 }
