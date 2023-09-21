c8bffadfeef7c1f4b7ce2727faedb2cf8f43d707
Zaki Ahmad <zakiahmad42661@gmail.com>
2023-09-21 17:53:55 -0400

    enabled server actions

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,24 +5,25 @@ import SectionHeading from './SectionHeading'
 import { FaPaperPlane } from 'react-icons/fa';
 import { useSectionInView } from '@/lib/hooks';
 import {motion} from 'framer-motion'
+import { sendEmail } from '@/actions/sendEmail';
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
 
+  
+
   return (
-    <motion.section 
-      ref={ref} 
-      id="contact" 
+    <motion.section
+      ref={ref}
+      id="contact"
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
       initial={{ opacity: 0 }}
-      whileInView={
-        {opacity: 1}
-      }
-      transition={{duration: 1}}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
       viewport={{
         once: true
       }}
-      >
+    >
       <SectionHeading>Contact Me</SectionHeading>
 
       <p className="text-gray-700 -mt-6">
@@ -33,9 +34,14 @@ export default function Contact() {
         or through this form
       </p>
 
-      <form className="mt-10 flex flex-col">
-        <input className="h-14 px-4 rounded-lg borderBlack" type="email" placeholder='Your email'/>
-        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder='Your Message'/>
+      <form className="mt-10 flex flex-col" 
+        action={async (formData) => {
+          console.log(formData.get("senderEmail"))
+          console.log(formData.get("message"))
+          await sendEmail(formData)
+        }}>
+        <input className="h-14 px-4 rounded-lg borderBlack" name="senderEmail" type="email" required maxLength={100} placeholder="Your email" />
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" name="message" required maxLength={500} placeholder="Your Message" />
         <button
           className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white 
           rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
