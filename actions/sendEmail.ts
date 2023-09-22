cac0d221561479f59d67439f50434cf1be83ef35
Zaki Ahmad <zakiahmad42661@gmail.com>
2023-09-21 20:15:25 -0400

    finished contact form backend, added react toast

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,48 @@
 "use server";
 
+import { validateString } from "@/lib/utils";
+import { getErrorMessage } from "@/lib/utils";
+import { Resend } from "resend";
+import ContactFormEmail from "@/email/ContactFormEmail";
+import React from "react";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+
+
 export const sendEmail = async (formData: FormData) => {
-  console.log("server")
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+  const message = formData.get('message');
+  const senderEmail = formData.get('senderEmail');
+
+  if (!validateString(senderEmail, 500)){
+    return {
+      error: 'Invalid sender email'
+    }
+  }
+  if (!validateString(message,5000)){
+    return {
+      error: 'Invalid message'
+    }
+  }
+
+  let data;
+  try{
+    data = await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: 'zakiahmad42661@gmail.com',
+      subject: 'Hello from Resend',
+      reply_to: senderEmail as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      })
+    })
+  } catch(error: unknown){
+
+    return {
+      error: getErrorMessage(error)
+    }
+  }
+
+  return {data}
 };
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,16 +2,15 @@
 
 import React from 'react'
 import SectionHeading from './SectionHeading'
-import { FaPaperPlane } from 'react-icons/fa';
 import { useSectionInView } from '@/lib/hooks';
 import {motion} from 'framer-motion'
 import { sendEmail } from '@/actions/sendEmail';
+import SubmitBtn from './SubmitBtn';
+import toast from 'react-hot-toast';
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
-
   
-
   return (
     <motion.section
       ref={ref}
@@ -36,25 +35,17 @@ export default function Contact() {
 
       <form className="mt-10 flex flex-col" 
         action={async (formData) => {
-          console.log(formData.get("senderEmail"))
-          console.log(formData.get("message"))
-          await sendEmail(formData)
+          const {data, error} = await sendEmail(formData)
+          if (error) {
+            toast.error(error)
+            return;
+          }
+
+          toast.success("Email sent successfully!")
         }}>
         <input className="h-14 px-4 rounded-lg borderBlack" name="senderEmail" type="email" required maxLength={100} placeholder="Your email" />
-        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" name="message" required maxLength={500} placeholder="Your Message" />
-        <button
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white 
-          rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
-          type="submit"
-        >
-          Submit{" "}
-          <FaPaperPlane
-            className="text-xs 
-          opacity-70 transition-all 
-          group-hover:translate-x-1
-          group-hover:-translate-y-1"
-          />
-        </button>
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" name="message" required maxLength={5000} placeholder="Your Message" />
+        <SubmitBtn/>
       </form>
     </motion.section>
   );
