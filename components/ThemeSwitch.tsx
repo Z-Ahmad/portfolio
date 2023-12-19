6465215a9c506af14e298514117b4103891dd1dd
Zaki Ahmad <zakiahmad42661@gmail.com>
2023-12-19 16:02:50 -0500

    Add animation to ThemeSwitch button until it is clicked once

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -6,10 +6,14 @@ import { BsMoon, BsSun } from 'react-icons/bs'
 
 export default function ThemeSwitch() {
   const {theme, toggleTheme} = useTheme()
+  const [clicked, setClicked] = React.useState(false)
 
   return (
-    <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-black border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:border-white dark:border-opacity-30'
-    onClick={toggleTheme}
+    <button className={`fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-black border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:border-white dark:border-opacity-30
+    ${!clicked ? "animate-bounce" : ""}`
+  
+    }
+    onClick={()=>{toggleTheme(); setClicked(true)}}
     >
       {theme === "light" ? <BsSun/> : <BsMoon/>}
     </button>
